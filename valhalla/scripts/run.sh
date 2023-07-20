661632a750f87c9bf073c0952aca6ccd985dc61e
smohiudd <saadiq@developmentseed.org>
2023-07-19 22:00:34 -0600

    docker compose chances

--- valhalla/scripts/run.sh
+++ valhalla/scripts/run.sh
@@ -7,22 +7,22 @@ set -e
 
 # we need to either run commands that create files with or without sudo (depends if the image was built with a UID/GID other than 0)
 run_cmd() {
-  if [[ "${VALHALLA_UID}" == "59999" ]] && [[ "${VALHALLA_GID}" == "59999" ]]; then
-    # -E preserves the env vars
-    sudo -E $1 || exit 1
+  if [[ $(id --user) == "59999" ]] && [[ $(id --group) == "59999" ]]; then
+    # -E preserves the env vars, but some are still nulled for security reasons
+    # use "env" to preserve them
+    $cmd_prefix sudo -E env LD_LIBRARY_PATH=$LD_LIBRARY_PATH $1 || exit 1
   else
-    $1 || exit 1
+    $cmd_prefix $1 || exit 1
   fi
 }
 
 do_build_tar() {
-  if ! test -d $TILE_DIR; then
-    echo "ERROR: No tiles found. Did you forget to build tiles?"
-    exit 1
-  fi
-  
   if ([[ ${build_tar} == "True" && ! -f $TILE_TAR ]]) || [[ ${build_tar} == "Force" ]]; then
-    run_cmd "valhalla_build_extract -c ${CONFIG_FILE} -v"
+    options="-c ${CONFIG_FILE} -v"
+    if ! [[ -z ${traffic_name} ]]; then
+      options="${options} -t"
+    fi
+    run_cmd "valhalla_build_extract ${options}"
   fi
 }
 
@@ -31,7 +31,7 @@ dir_owner=$(stat --format '%U' "${CUSTOM_FILES}")
 echo ""
 echo "INFO: Running container with user $(whoami) UID $(id --user) and GID $(id --group)."
 if [[ ${dir_owner} == "root" ]]; then
-  if [[ "${VALHALLA_UID}" != "59999" ]] || [[ "${VALHALLA_GID}" != "59999" ]]; then
+  if [[ $(id --user) != "59999" ]] || [[ $(id --group) != "59999" ]]; then
     echo "ERROR: If you run with custom UID or GID you have to create the mapped directory to the container's /custom_files manually before starting the image"
     exit 1
   fi
@@ -40,16 +40,20 @@ echo ""
 
 # the env vars with True default are set in the dockerfile, others are evaluated in configure_valhalla.sh
 if [[ -z $server_threads ]]; then
-  server_threads=$(nproc)
+  export server_threads=$(nproc)
 fi
+
 if [[ -z $build_tar ]]; then
   build_tar="True"
 fi
+if [[ -z $serve_tiles ]]; then
+  serve_tiles="True"
+fi
 
-# evaluate CMD 
+# evaluate CMD
 if [[ $1 == "build_tiles" ]]; then
 
-  run_cmd "/valhalla/scripts/configure_valhalla.sh ${CONFIG_FILE} ${CUSTOM_FILES} ${TILE_DIR} ${TILE_TAR}" 
+  run_cmd "/valhalla/scripts/configure_valhalla.sh ${CONFIG_FILE} ${CUSTOM_FILES} ${TILE_DIR} ${TILE_TAR}"
   # tar tiles unless not wanted
   if [[ "$build_tar" == "True" ]] || [[ ${build_tar} == "Force" ]]; then
     do_build_tar
@@ -58,7 +62,7 @@ if [[ $1 == "build_tiles" ]]; then
   fi
 
   # lazy workaround, not sure what's wrong when using the run_cmd function..
-  if [[ "${VALHALLA_UID}" == "59999" ]] && [[ "${VALHALLA_GID}" == "59999" ]]; then
+  if [[ $(id --user) == "59999" ]] && [[ $(id --group) == "59999" ]]; then
     echo "WARNING: User $(whoami) is running with sudo privileges. Try building the image with a host user's UID & GID."
     # set 775/664 permissions on all created files
     sudo find "${CUSTOM_FILES}" -type d -exec chmod 775 {} \;
@@ -68,16 +72,19 @@ if [[ $1 == "build_tiles" ]]; then
     find "${CUSTOM_FILES}" -type f -exec chmod 664 {} \;
   fi
 
-  if test -f ${CONFIG_FILE}; then
-    echo "INFO: Found config file. Starting valhalla service!"
-    run_cmd "valhalla_service ${CONFIG_FILE} ${server_threads}"
-  else
-    echo "WARNING: No config found!"
-  fi
+  if [[ ${serve_tiles} == "True" ]]; then
+    if test -f ${CONFIG_FILE}; then
+      echo "INFO: Found config file. Starting valhalla service!"
+      cmd_prefix=exec run_cmd "valhalla_service ${CONFIG_FILE} ${server_threads}"
+    else
+      echo "WARNING: No config found!"
+    fi
 
-  # Keep docker running easy
-  exec "$@"
-  
+    # Keep docker running easy
+    exec "$@"
+  else
+    echo "INFO: Not serving tiles. Exiting."
+  fi
 elif [[ $1 == "tar_tiles" ]]; then
   do_build_tar
 else
